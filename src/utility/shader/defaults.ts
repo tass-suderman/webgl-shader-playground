81162d607b81e93f56bf38d350e16d4d190e1440
tass-suderman <github@tass-suderman.ca>
2026-05-24 15:16:43 -0600

    feat: changed defaults a bit

--- src/utility/shader/defaults.ts
+++ src/utility/shader/defaults.ts
@@ -1,16 +1,6 @@
-export const DEFAULT_SHADER_TITLE = 'Fragment Shader (GLSL)'
+export const DEFAULT_SHADER_TITLE = 'Kaleidoscopic Camera'
 
-export const DEFAULT_SHADER = `// WebGL Shader Playground
-// Compatible with ShaderToy uniforms:
-//   iTime       - shader playback time (in seconds)
-//   iResolution - viewport resolution (in pixels)
-//   iMouse      - mouse pixel coords (xy: current, zw: click)
-//   iFrame      - shader playback frame
-//   iChannel0   - webcam texture (if enabled)
-//   iChannel1   - microphone / system audio frequency texture (if enabled)
-//   iChannel2   - Strudel live-coding audio frequency texture (if enabled)
-
-precision highp float;
+export const DEFAULT_SHADER = `precision highp float;
 
 uniform vec2 iResolution;
 uniform float iTime;
@@ -24,36 +14,34 @@ uniform sampler2D iChannel2;
 uniform bool iChannel2Enabled;
 
 void main() {
-  vec2 uv = gl_FragCoord.xy / iResolution.xy;
-  
-  // Animated gradient
-  vec3 col = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0.0, 2.0, 4.0));
-  
-  // Add some wave distortion
-  float wave = sin(uv.x * 10.0 + iTime * 2.0) * 0.05;
-  col += vec3(wave, wave * 0.5, wave * 2.0);
-  
-  // Sample webcam if enabled (iChannel0)
-  if (iChannel0Enabled) {
-    vec2 camUV = vec2(uv.x, 1.0 - uv.y);
-    vec4 camColor = texture2D(iChannel0, camUV);
-    col = mix(col, camColor.rgb, 0.5);
-  }
-  
-  // Sample audio frequency data if enabled (iChannel1)
-  // iChannel1 is a 1D texture where each texel's red channel is a frequency bin
-  if (iChannel1Enabled) {
-    float freq = texture2D(iChannel1, vec2(uv.x, 0.5)).r;
-    col += vec3(0.0, freq * 0.3, freq * 0.6);
-  }
-  
-  // Sample Strudel audio frequency data if enabled (iChannel2)
-  // iChannel2 works the same as iChannel1 – 1D frequency texture from Strudel
-  if (iChannel2Enabled) {
-    float freq2 = texture2D(iChannel2, vec2(uv.x, 0.5)).r;
-    col += vec3(freq2 * 0.6, freq2 * 0.1, freq2 * 0.4);
-  }
-  
-  gl_FragColor = vec4(col, 1.0);
+  vec2 pos = gl_FragCoord.xy / iResolution.xy;
+
+
+  pos -= .5;
+
+ float symmetry_count = 8.0;
+    float radius = sqrt(pow(pos.x, 2.0) + pow(pos.y, 2.0));
+    float angle = atan(pos.y, pos.x);
+    float wedge_size = (2.0 * 3.14) / symmetry_count;
+    float angle_r = mod(angle, wedge_size);
+    float angle_folded = abs(angle_r - (wedge_size/2.0));
+    float x_folded = radius * cos(angle_folded);
+    float y_folded = radius * sin(angle_folded);
+
+    float sound = 0.;
+    float freq = 0.;
+    if(iChannel1Enabled)
+    {
+        sound = texture2D(iChannel1, vec2(x_folded, 0.25)).x;
+        freq = texture2D(iChannel1, vec2(x_folded, 0.75)).x;
+    } else if (iChannel2Enabled)
+    {
+        sound = texture2D(iChannel2, vec2(x_folded, 0.25)).x;
+        freq = texture2D(iChannel2, vec2(x_folded, 0.75)).x;
+    }
+
+    vec4 cam = texture2D(iChannel0, vec2((sin(x_folded + sound/2.)), (1.-y_folded - freq/3.)));
+
+  gl_FragColor = cam;
 }
 `
--- src/utility/strudel/defaults.ts
+++ src/utility/strudel/defaults.ts
@@ -1,6 +1,24 @@
 export const DEFAULT_STRUDEL_TITLE = 'Strudel Pattern'
 
-export const DEFAULT_STRUDEL_CODE = `// Strudel live-coding pattern
-// Alt+Enter to play, Alt+. to pause
-note("c3 [e3 g3] b3 [g3 e3]").sound("sawtooth").lpf(800).lpenv(2).slow(2)`
+export const DEFAULT_STRUDEL_CODE = `
+// Inspired by Switch Angel's Melodic Drum n Bass tutorial 
+// https://www.youtube.com/watch?v=aPsq5nqvhxg
+register('rlpf', (x,pat) => {return pat.lpf(pure(x).mul(12).pow(4))})
+setGainCurve(x => Math.pow(x,2))
+setcpm(170/4)
+
+$drums: stack(
+  s("bd:1").beat("0,7?,10",16).duck("3:4:5"),
+  s("sd909:2").beat("4,12",16),
+  s("hh:4!8")
+  )
+  // .rib(0,1/4)
+  .orbit(2)._scope()
+
+$bass: s("supersaw!8")
+  .note("<c# f d# [d# a#2]>/2".sub("[12 0]")).fast(4).orbit(3)
+  .rlpf(slider(1)).lpenv("2")
+
+$riser: s("pulse!16").dec(.1).fm(time).fmh(time).orbit(5)
+`
 
